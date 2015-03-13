e42411e3c2e631c5843286b37470d4cc46d9aa9e
Jeff <jeoffrey.jardin@gmail.com>
2015-03-13 09:19:30 +0100

    add all fichier

---
+++ projet_tmp/src/hexalib/HexagonPanel.java
@@ -0,0 +1,52 @@
+package hexalib;
+
+import hexalib.Hexalib.Direction;
+import hexalib.Hexalib.HexagonView;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.util.HashMap;
+
+public class HexagonPanel implements HexagonView {
+	private  Color color=Color.blue;
+	private HashMap<Direction, Color> colorEdge= new HashMap<>();
+	private int cx[];
+	private int cy[];
+	@Override
+	public void setColor(Color color) {
+		this.color=color;
+	}
+
+	public void setCx( int ... cx){
+		this.cx=cx;
+
+	}
+	public void setCy(int ... cy){
+		this.cy=cy;
+	}
+	@Override
+	public void setEdge(Direction edge, Color color) {
+		colorEdge.put(edge, color);
+
+	}
+	@Override
+	public Color getColor() {
+		return this.color;
+	}
+	@Override
+	public Color getEdgeColot(Direction edge) {
+		return this.colorEdge.get(edge);
+	}
+	public void drawHex(Graphics2D g2) {
+		Polygon poly = new Polygon(cx,cy,6);
+		g2.setColor(this.color);
+		g2.fillPolygon(poly);
+		//g2.setColor(this.color);
+		g2.setColor(Color.BLACK);
+		g2.drawPolygon(poly);
+	}
+
+	
+
+}
---
+++ projet_tmp/src/hexalib/Point.java
@@ -0,0 +1,14 @@
+package hexalib;
+
+public class Point {
+
+	public final int x;
+	public final int y;
+	public final int z;
+	
+	public Point(int x,int y,int z) {
+		this.x=x;
+		this.y=y;
+		this.z=z;
+	}
+}
